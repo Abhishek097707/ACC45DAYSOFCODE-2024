8b2feb29d9cf4b530fde35587cddc7ee37a9206d
Abhishek097707 <abhishekushwah2003@gmail.com>
2024-10-25 11:19:33 +0530

    Add files via upload

---
+++ Day-23_RECENTCONT.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+
+        int start38_count = 0, ltime108_count = 0;
+        char contest_code[20];
+
+        for (int i = 0; i < N; i++) {
+            scanf("%s", contest_code);
+
+            if (strcmp(contest_code, "START38") == 0) {
+                start38_count++;
+            } else if (strcmp(contest_code, "LTIME108") == 0) {
+                ltime108_count++;
+            }
+        }
+
+        printf("%d %d\n", start38_count, ltime108_count);
+    }
+
+    return 0;
+}
